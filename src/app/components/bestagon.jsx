f0c462dd017ee5d290b694990ee46cab104e5850
Mottlly <147268663+Mottlly@users.noreply.github.com>
2025-03-11 14:16:07 -0300

    cactus implementation begins

--- src/app/components/bestagon.jsx
+++ src/app/components/bestagon.jsx
@@ -1,23 +1,21 @@
 "use client";
 
-import React, { useState } from "react"; // Ensure useState is imported
+import React, { useState, useMemo } from "react";
 import { Edges } from "@react-three/drei";
+import Cactus from "../components/cactus";
 
 const Bestagon = ({ position, color }) => {
-  const [selected, setSelected] = useState(false); // Ensure useState is used properly
+  const [selected, setSelected] = useState(false);
 
   const handleClick = () => {
-    setSelected((prev) => !prev); // Toggle selected state
+    setSelected((prev) => !prev);
   };
 
   return (
     <mesh position={position} onClick={handleClick}>
-      {" "}
-      {/* Handle clicks correctly */}
       <cylinderGeometry args={[1, 1, 0.5, 6]} />
       <meshStandardMaterial color={color} />
-      <Edges color={selected ? "lime" : "white"} />{" "}
-      {/* Changes outline on click */}
+      <Edges color={selected ? "lime" : "white"} />
     </mesh>
   );
 };
