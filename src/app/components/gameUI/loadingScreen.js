e298d0d7ac422ff3fde86b6c12a4719416f9fb01
Mottlly <147268663+Mottlly@users.noreply.github.com>
2025-06-02 22:22:32 -0300

    loading screen language files update

--- src/app/components/gameUI/loadingScreen.js
+++ src/app/components/gameUI/loadingScreen.js
@@ -1,31 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { loadingScreenStyles } from "@/library/styles/stylesIndex";
 
 export default function LoadingScreen() {
-  const messages = [
-    "Thawing out the crew...",
-    "Preparing for landing on [unknown]...",
-    "Deploying heat shields...",
-    "Analyzing local Flora and Fauna...",
-    "Identifying mineral deposits...",
-    "Downloading alien enviornment safety protocols...",
-    "Error(503): Server Unavailable, contact IT support...",
-    "Searching for optimal landing site...",
-    "Analyzing atmospheric conditions...",
-    "CAUTION: dense atmospheric conditions...",
-    "WARNING: heat shields failing...",
-    "WARNING: 47.867% of emergency supplies lost...",
-    "WARNING: Emergency fusion cells lost...",
-    "Diverting energy to secondary heat shields...",
-    "WARNING Life support systems failing, diagnosing...",
-    "Life Support Systems failing due to: energy deficit",
-    "Cancelling crew thaw...",
-    "Informing Corporate of lost assets...",
-    "Subtracting value of lost assets from your bank account...",
-    "Informing next of kin of likely death...",
-    "Error(404): No next of kin found...",
-    "Transferring crew assets to corporate account for safekeeping...",
-  ];
+  const { t } = useTranslation("common");
+  const messages = t("loadingMessages", { returnObjects: true });
 
   const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
 
